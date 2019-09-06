7557e5038af8147201576b7b07c68d0a90efb86e
shariq95 <38542724+shariq95@users.noreply.github.com>
2019-09-06 00:11:25 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import "./App.css";
-import { recipes } from "./tempList";
 import RecipeList from "./components/RecipeList";
 import RecipeDetails from "./components/RecipeDetails";
 
